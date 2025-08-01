e0d105518df1a635a2ad2731f15d211d860e280c
Ivan Shyrokykh <ivanshyrokykh@MacBook-Air-Ivan.local>
2025-08-01 19:02:33 +0300

    Add minista dev, prod config

---
+++ minista.prod.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "minista"
+import createConfig from "./minista.common.config";
+
+export default defineConfig(
+    createConfig({
+        base: '/streamvibe/'
+    })
+);
